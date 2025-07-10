cc4e7e8537f38d17a07e91ea7a1f673cefadb9db
DanHuy081 <bit220078@st.cmcu.edu.vn>
2025-07-11 02:47:37 +0700

    them tinh nang up video va xem video

---
+++ .angular/cache/19.2.15/frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-EKXUFEYM.js";
+import "./chunk-BET75IHR.js";
+import "./chunk-C5IWY22B.js";
+import "./chunk-TM254JPZ.js";
+import "./chunk-7MRS52LK.js";
+import "./chunk-BK2CO3JY.js";
+import "./chunk-H7T6ZUR2.js";
+import "./chunk-KQFPNTK3.js";
+import "./chunk-XOB36AR6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { authGuard } from './auth.guard';
+import { VideoUploadComponent } from './pages/video-upload/video-upload.component';
+import { LearnOnlineComponent } from './pages/learn-online/learn-online.component';
+
 
 export const routes: Routes = [
   { path: '', component: LoginComponent, data: { title: 'Trang đăng nhập' } },
   { path: 'login', component: LoginComponent, data: { title: 'Trang đăng nhập' } },
   { path: 'signup', component: SignupComponent, data: { title: 'Đăng ký tài khoản' } },
   { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' }, canActivate: [authGuard] },
+  { path: 'video', redirectTo: 'upload', pathMatch: 'full' }, // mặc định chuyển hướng
+  { path: 'upload', component: VideoUploadComponent, data: { title: 'Tải video lên' } },
+  { path: 'classroom', component: LearnOnlineComponent, data: { title: 'Phòng học' } }
 ];
